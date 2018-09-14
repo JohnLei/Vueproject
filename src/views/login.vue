3f05e593e047e32f3a541457985de40e0b205993
JohnLei <862639456@qq.com>
2018-09-14 14:55:01 +0800

    登录页完成

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="login">
     <el-form ref="form" :model="form" class="container" :rules="rules">
-        <el-form-item>
-            <div class="avatar">
-                <img src="../assets/avatar.jpg" alt="">
-            </div>
-        </el-form-item>
+    <el-form-item>
+        <div class="avatar">
+            <img src="../assets/avatar.jpg" alt="">
+        </div>
+    </el-form-item>
     <el-form-item prop="name">
     <el-input v-model="form.name" placeholder="账户" prefix-icon="myicon myicon-user"></el-input>
     </el-form-item>
     <el-form-item prop="password">
-    <el-input v-model="form.password" placeholder="密码" prefix-icon="myicon myicon-key"></el-input>
+    <el-input v-model="form.password" placeholder="密码" prefix-icon="myicon myicon-key" type="password"></el-input>
     </el-form-item>
     <el-form-item>
         <el-button type="primary" class="login-btn">登录</el-button>
@@ -19,25 +19,24 @@
     </div>
 </template>
 <script>
-    export default {
-        data () {
-            return {
-                form:{
-                    name:'',
-                    password:''
-                },
-                rules:{
-                    name:[
-                        
-                        {required: true, message: '请输入用户名', trigger: 'blur'}    
-                    ],
-                    password:[
-                        {required: true, message: '请输入密码', trigger: 'blur'}  
-                    ]
-                }
+export default {
+    data () {
+        return {
+          form: {
+            name:'',
+            password:''
+          },
+          rules:{
+              name:[
+                  {required: true, message: '请输入用户名', trigger: 'blur'}
+              ],
+              password:[
+                  {required: true, message: '请输入密码', trigger: 'blur'}
+              ]
             }
         }
     }
+}
 </script>
 
 <style lang="scss" scoped>
